a08928f128e0d68d11153f6dce3e2559fd7fb7e0
Alister Lewis-Bowen <alister@different.com>
2017-04-27 17:55:36 -0400

    add projects

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Scripts used in conjuction with VMware products
 
 The [ESXi directory](ESXi) contains scripts to help work with [VMware ESXi](https://www.vmware.com/products/vsphere-hypervisor).
 
+The [Photon Controller directory](photon_controller) contains scripts to work with [VMware Photon Controller](https://vmware.github.io/photon-controller/)
+
 ## Contribution
 If you've stumbled upon this project and wish to contribute, please
 [let me know](mailto:alister@different.com)
--- photon_controller/pc_populate.sh
+++ photon_controller/pc_populate.sh
@@ -35,29 +35,64 @@ echo
 NUM_TENANTS=10
 TENANT_NAMES=$(seq $NUM_TENANTS | xargs -Iz "$PWD/generate_word_string.sh")
 
-set -x
-
-# Inconsistent option to define the name. Probably should be --name ...
-photon -n tenant create 'Test-Tenant' --limits "\
+LIMITS_LARGE="\
 vm 20000 COUNT, vm.cost 20000 COUNT, vm.cpu 20000 COUNT, vm.memory 20000 GB, \
 ephemeral-disk 20000 COUNT, ephemeral-disk.capacity 20000 GB, ephemeral-disk.cost 20000 GB, \
 persistent-disk 20000 COUNT, persistent-disk.capacity 20000 GB, persistent-disk.cost 20000 GB, \
 storage.LOCAL_VMFS 20000 COUNT, \
 sdn.floatingip.size 20000 COUNT"
 
-for tenant in $TENANT_NAMES; do
-    photon -n tenant create "$tenant" --limits "\
+LIMITS_SMALL="\
 vm 100 COUNT, vm.cost 100 COUNT, vm.cpu 100 COUNT, vm.memory 100 GB, \
 ephemeral-disk 100 COUNT, ephemeral-disk.capacity 100 GB, ephemeral-disk.cost 100 GB, \
 persistent-disk 100 COUNT, persistent-disk.capacity 100 GB, persistent-disk.cost 100 GB, \
 storage.LOCAL_VMFS 100 COUNT, \
 sdn.floatingip.size 100 COUNT"
 
+set -x
+
+# Inconsistent option to define the name. Probably should be --name ...
+photon -n tenant create 'Test-Tenant' --limits "$LIMITS_LARGE"
+
+# Should be allowed to identify an object by it's name OR ID...
+photon -n project create 'Test-Project' --tenant 'Test-Tenant' -limits "$LIMITS_LARGE"
+
+# TODO: Create subnets in default router
+
+# TODO: Create additional routers and subnets
+
+# TODO: Create VMs
+
+# TODO: Create Services
+
+#
+# Create filler Tenants
+#
+
+for tenant in $TENANT_NAMES; do
+
+    photon -n tenant create "$tenant" --limits "$LIMITS_SMALL"
 done
+
+#
+# Create filler projects
+#
 
 # Should be easier to get the name of an object...
 for tenant in $(photon tenant list | grep -E "\w{8}-\w{4}-\w{4}-\w{4}-\w{12}" | awk '{print $2}'); do
-    echo "$tenant"
-    # TODO create projects
+
+    # Should be allowed to identify an object by it's name OR ID...
+    photon tenant set "$tenant"
+
+    set +x
+    NUM_PROJECTS=$(( (RANDOM%10)+2 ))
+    PROJECT_NAMES=$(seq $NUM_PROJECTS | xargs -Iz "$PWD/generate_word_string.sh")
+    set -x
+
+    for project in $PROJECT_NAMES; do
+
+        photon -n project create "$project" --limits "$LIMITS_SMALL"
+    done
+
 done
 
