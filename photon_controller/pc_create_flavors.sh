598764cf1e9c399c5b3326d24c30d224f5bd5b81
Alister Lewis-Bowen <alister@different.com>
2017-04-27 13:03:33 -0400

    add comments to flavor script

--- photon_controller/pc_create_flavors.sh
+++ photon_controller/pc_create_flavors.sh
@@ -15,13 +15,15 @@ type photon &> /dev/null || {
 
 photon deployment list &> /dev/null || {
     echo "Set your Photon Platform target and log into it, e.g."
-    echo "  photon target set -c https://172.20.0.55:443"
-    echo "  photon target login --username administrator@photon.local.v --password 'P@s\$w0rd'"
+    echo "  photon target set -c https://192.168.0.10:443"
+    echo "  photon target login --username administrator@local --password 'passwd'"
     exit 1
 }
 
 set -x
 
+# Strange that -n can mean both non-interactive AND name of the flavor
+# Even though these options are positional, this is very ambiguous for the user...
 photon -n flavor create --kind vm --name tiny-vm --cost "vm.cpu 1 COUNT, vm.memory 512 MB, vm.cost 1 COUNT"
 photon -n flavor create --kind vm --name small-vm --cost "vm.cpu 1 COUNT, vm.memory 1 MB, vm.cost 2 COUNT"
 photon -n flavor create --kind vm --name medium-vm --cost "vm.cpu 2 COUNT, vm.memory 2 MB, vm.cost 3 COUNT"
