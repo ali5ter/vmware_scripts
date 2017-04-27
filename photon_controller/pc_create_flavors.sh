68131031447b2dbedc55003f716c37773a4d348c
Alister Lewis-Bowen <alister@different.com>
2017-04-27 11:51:32 -0400

    create flavors

---
+++ photon_controller/pc_create_flavors.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# @file pc_create_flavors.sh
+# Create some useful Photon Controller Flavors
+# @see https://github.com/vmware/photon-controller/wiki/Command-Line-Cheat-Sheet
+# @author Alister Lewis-Bowen <alister@lewis-bowen.org>
+
+type photon &> /dev/null || {
+    echo "Please install photon CLI by downloading it from the release page at"
+    echo "https://github.com/vmware/photon-controller/releases"
+    echo "Add execute permissions and move into your path, e.g."
+    echo "  chmod +x ~/Downloads/photon-darwin64-1.2-dd9d360"
+    echo "  sudo mv ~/Downloads/photon-darwin64-1.2-dd9d360 /usr/local/bin/photon"
+    exit 1
+}
+
+photon deployment list &> /dev/null || {
+    echo "Set your Photon Platform target and log into it, e.g."
+    echo "  photon target set -c https://172.20.0.55:443"
+    echo "  photon target login --username administrator@photon.local.v --password 'P@s\$w0rd'"
+    exit 1
+}
+
+set -x
+
+photon -n flavor create --kind vm --name tiny-vm --cost "vm.cpu 1 COUNT, vm.memory 512 MB, vm.cost 1 COUNT"
+photon -n flavor create --kind vm --name small-vm --cost "vm.cpu 1 COUNT, vm.memory 1 MB, vm.cost 2 COUNT"
+photon -n flavor create --kind vm --name medium-vm --cost "vm.cpu 2 COUNT, vm.memory 2 MB, vm.cost 3 COUNT"
+photon -n flavor create --kind vm --name tiny-vm --cost "vm.cpu 2 COUNT, vm.memory 4 MB, vm.cost 4 COUNT"
+
+photon -n flavor create --kind ephemeral-disk --name vm-disk --cost "ephemeral-disk 1 COUNT, ephemeral-disk.flavor.vm-disk 1 COUNT, ephemeral-disk.cost 1 COUNT"
+photon -n flavor create --kind ephemeral-disk --name cluster-vm-disk --cost "ephemeral-disk 1 COUNT, ephemeral-disk.flavor.cluster-vm-disk 1 COUNT, ephemeral-disk.cost 1 COUNT"
+
+photon -n flavor create --kind persistent-disk --name vm-disk --cost "persistent-disk 1 COUNT, persistent-disk.flavor.vm-disk 1 COUNT, persistent-disk.cost 1 COUNT"
+
+photon flavor list
