a3b7cd8a7c8af8ee6f0414c869fa699948e0b8a2
Alister Lewis-Bowen <alister@different.com>
2017-04-27 21:09:39 -0400

    add ability to change delimiter

--- photon_controller/generate_word_string.sh
+++ photon_controller/generate_word_string.sh
@@ -6,13 +6,14 @@
 WORDFILE="/usr/share/dict/words"
 TOTAL_WORDS=$(awk 'NF!=0 {++c} END {print c}' $WORDFILE)
 NUM_WORDS=${1:-3}
+DELIM=${2:--}
 
 string=''
 for i in $(seq $NUM_WORDS); do
     rnum=$((RANDOM%TOTAL_WORDS+1))
     word=$(sed -n "$rnum p" $WORDFILE)
     if [ "$string" == "" ]; then string="$word"
-    else string="$string-$word"
+    else string="${string}${DELIM}$word"
     fi
 done
 echo "$string" | tr '[:upper:]' '[:lower:]'
