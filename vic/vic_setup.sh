80caed08ca4c260d3b2482b58f98724857e6057c
Alister Lewis-Bowen <bowen@vmware.com>
2017-08-30 08:34:35 -0400

    fix credential store and use as prompt defaults

--- vic/vic_setup.sh
+++ vic/vic_setup.sh
@@ -4,6 +4,9 @@
 
 set -e
 
+STORE=~/.vic_scripts_config
+source "$STORE" ## to use as defaults
+
 # Download and position vic-machine build
 
 BUILD=vic_13400
@@ -24,57 +27,67 @@ echo
 
 # Determin which platform binary to access
 
-CMD="$DIR/vic-machine-"
+VIC_CLI="$DIR/vic-machine-"
 case "$OSTYPE" in
-    darwin*) CMD="${CMD}darwin" ;;
-    linux*)  CMD="${CMD}linux" ;;
+    darwin*) VIC_CLI="${VIC_CLI}darwin" ;;
+    linux*)  VIC_CLI="${VIC_CLI}linux" ;;
     *)
         echo "$OSTYPE is not a supported operating system"
         exit 1
         ;;
 esac
 shopt -s expand_aliases
-alias vic-machine="$CMD"
+alias vic-machine="$VIC_CLI"
 
 # Prompt for VC and credentials
 
 echo
-read -rp "Enter the address of your vCenter Server or ESXi host: " HOST
+read -rp "Enter the address of your vCenter Server or ESXi host [$VIC_HOST]: "
+[ ! -z "$REPLY" ] && $VIC_HOST="$REPLY"
 
 echo
-read -rp "Enter the username you use to admnistrate this: " USERNAME
+read -rp "Enter the username you use to admnistrate this [$VIC_USER]: "
+[ ! -z "$REPLY" ] && $VIC_USER="$REPLY"
 
 echo 
-read -rp "Enter the password for this username: " PASSWD
+read -rp "Enter the password for this username [$VIC_PASS]: "
+[ ! -z "$REPLY" ] && $VIC_PASS="$REPLY"
 
-# Get cert thumbprint and create auth strig
+# Get cert thumbprint and create auth string
 
-THUMB=$($PWD/../vsphere/show_thumbprint.sh $HOST| cut -d"=" -f 2)
-AUTH='--target '"$HOST"' --user '"$USERNAME"' --password '"$PASSWD"' --thumbprint '"$THUMB"
+VIC_THUMB=$($PWD/../vsphere/show_thumbprint.sh $VIC_HOST| cut -d"=" -f 2)
+VIC_AUTH='--target '"$VIC_HOST"' --user '"$VIC_USER"' --password '"$VIC_PASS"' --thumbprint '"$VIC_THUMB"
 
 # Configure firewall for VCH end-point access
 
 echo 
-read -rp "Enter the Cluster name you will use for VCHs: " CLUSTER
+read -rp "Enter the Cluster name you will use for VCHs [$VIC_CLUSTER]: "
+[ ! -z "$REPLY" ] && $VIC_CLUSTER="$REPLY"
 
-vic-machine update firewall $AUTH --compute-resource "$CLUSTER" --allow
+echo
+echo "Configuring vCenter host firewall rules for VIC..."
+vic-machine update firewall $VIC_AUTH --compute-resource "$VIC_CLUSTER" --allow
 
 # Store configuration
 
-STORE=~/.vic_scripts_config
 [ -f $STORE ] && rm -f "$STORE"
 touch "$STORE"
-echo "VIC_CLI=$CMD" >> "$STORE"
-echo "VIC_HOST=$HOST" >> "$STORE"
-echo "VIC_USER=$USERNAME" >> "$STORE"
-echo "VIC_PASS=$PASSWD" >> "$STORE"
-echo "VIC_THUMB=$THUMB" >> "$STORE"
-echo "VIC_AUTH=$AUTH" >> "$STORE"
-echo "VIC_CLUSTER=$CLUSTER" >> "$STORE"
+echo "export VIC_CLI='$VIC_CLI'" >> "$STORE"
+echo "export VIC_HOST='$VIC_HOST'" >> "$STORE"
+echo "export VIC_USER='$VIC_USER'" >> "$STORE"
+echo "export VIC_PASS='$VIC_PASS'" >> "$STORE"
+echo "export VIC_THUMB='$VIC_THUMB'" >> "$STORE"
+echo "export VIC_AUTH='$VIC_AUTH'" >> "$STORE"
+echo "export VIC_CLUSTER='$VIC_CLUSTER'" >> "$STORE"
+source "$STORE"
 
 echo
 echo "Set up complete."
+echo
 echo "Use the following alias to access the vic-machine CLI:"
-echo "alias vic-machine=$CMD"
-echo "Source the $STORE file to have access to the following env vars:"
-echo env | grep VIC_
+echo "alias vic-machine='$VIC_CLI'"
+echo
+echo "Use the VIC_AUTH env var to include your vCenter credentials, e.g. vic-machine ls \$VIC_AUTH"
+echo
+echo "The $STORE file contains exports for the following env vars:"
+env | grep VIC_
