c77d3566f96d11cf123d14c272bfc7c6908b812f
Alister Lewis-Bowen <bowena@vmware.com>
2018-02-27 14:46:03 -0500

    CLI version check

--- cascade/cascade_config.sh
+++ cascade/cascade_config.sh
@@ -16,22 +16,58 @@ CLUSTER_PREFIX='alb'
 
 # helper functions -----------------------------------------------------------
 
-get_cli() {
-    echo "Please install cascade CLI by downloading from one of these URLs"
-    curl -s "$UI_URL"v1/cli | jq '.latest' | grep -v [{}]
-    echo "Add execute permissions and move into your path"
+get_CLI_url() {
+    local _os=''
+    case "$OSTYPE" in
+        darwin*)  _os='mac' ;; 
+        linux*)   _os='linux64' ;;
+        msys*)    _os='windows64' ;;
+        *)        return 1;;
+    esac
+    curl -s "$UI_URL"/v1/cli | jq -r --arg os "$_os" '.latest | .[$os]'
+    return 0
 }
-
-type cascade &> /dev/null || {
-    get_cli
-    exit 1
-}
-
-source "$PWD/cascade_completion.sh"
 
 heading() {
     printf '=%.0s' {1..79}
     echo -e "\n$1" | fold -s -w 79
     printf '=%.0s' {1..79}
     echo
+    return 0
 }
+
+check_version() {
+    local _tmp='/tmp/cascade'
+    local _latest_version=$(curl -s $(get_CLI_url) > $_tmp && chmod 755 $_tmp && $_tmp -v | awk '{print $3}')
+    local _current_version=$(cascade -v | awk '{print $3}')
+    [[ "$_current_version" != "$_latest_version" ]] && {
+        echo -e "\nThere is a new version ($_latest_version) of the Cascade CLI available"
+        echo "Move $_tmp to your path if you want the latest, e.g."
+        echo -e " \tmv $_tmp /usr/local/bin/cascade\n"
+    }
+    return 0
+}
+
+check_version_trigger() {
+    local _check=~/.cascade_version_check
+    if [[ -f "$_check" ]]; then
+        [[ "$(date '+%j')" != "$(cat $_check)" ]] && check_version
+    else
+        touch "$_check" && { date '+%j' > "$_check"; }
+        check_version
+    fi
+    return 0
+}
+
+# validate cascade commans ---------------------------------------------------
+
+type cascade &> /dev/null || {
+    echo "Please install cascade CLI by downloading it from"
+    get_CLI_url
+    echo "Add execute permissions and move into your path"
+    exit 1
+}
+
+check_version_trigger
+
+source "$PWD/cascade_completion.sh"
