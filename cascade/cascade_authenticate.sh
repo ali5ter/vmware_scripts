033ffd4c5152b419ec0112b71ae69e07986e9171
Alister Lewis-Bowen <bowena@vmware.com>
2018-05-14 09:57:00 -0400

    use new account and user subcommands to discover user and display groups

--- cascade/cascade_authenticate.sh
+++ cascade/cascade_authenticate.sh
@@ -10,21 +10,17 @@ source "$PWD/cascade_env.sh"
 ## Authentication state held in ~/cascade-cli/cascade-config but can't find
 ## a way to determine if I have an exired session, so just login in each time
 
-## no set option any more ?!!
-
-#erun cascade target set "$API_URL"
 erun cascade target login --i csp -t "$CSP_ORG_ID" -r "$TOKEN"
 
-## no get option any more ?!!
+CUSER=$(cascade -o json account show | jq -r '.sub')
+CUSER_ID=$(cascade -o json user show $CUSER | jq -r '.id')
 
-#erun cascade tenant get
+# Show groups your user is a member of ---------------------------------------
 
-## Unable to get the user identity I'm signed in as
+heading "Groups user $CUSER belongs to"
+cascade -o json account show | jq -r '.groups[]' | cut -d'\' -f2
 
 # navigate to correct scope --------------------------------------------------
-
-## Now that output is tabulated with lots more visual chars, it's easier to
-## output in json and use jq to extract attributes needed
 
 heading 'Navigate to correct project scope'
 erun cascade folder set $(cascade --output json folder list | jq -r '.items[] | .name' | grep -i "$FOLDER_REGEX")
--- cascade/cascade_config.sh
+++ cascade/cascade_config.sh
@@ -5,7 +5,6 @@
 
 UI_URL='https://cascade.cloud.vmware.com'
 API_URL='https://api.cascade-cloud.com'
-USER='bowena@vmware.com'
 TOKEN='8698fa83-f29f-4354-aeb9-ac6c4a44bd1d'
 CSP_ORG_ID='fa2c1d78-9f00-4e30-8268-4ab81862080d'
 
