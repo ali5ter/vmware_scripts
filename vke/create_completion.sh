fe17bcdb9f7c4be79cf301d6e0eafaae2d2f4fc1
Alister Lewis-Bowen <bowena@vmware.com>
2018-05-31 15:38:18 -0400

    script to generate completion script

---
+++ vke/create_completion.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# @file create_completion.sh
+# Create a bash completion script for vke using cli_taxo
+# @author Alister Lewis-Bowen <alister@lewis-bowen.org>
+
+set -e
+
+DIR="$PWD"
+
+## set up cli_taxo
+cd ~/tmp
+mkdir -p cli_taxo
+cd cli_taxo
+curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/cli_taxo.py
+curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/completion.tmpl
+curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/requirements.txt
+pip install -r requirements.txt
+chmod 755 cli_taxo.py
+
+## generate completion script and move into place
+./cli_taxo.py vke \
+    --commands-token '^COMMANDS:$' \
+    --commands-filter '^\s\s\s\s\s(?!-)(\S[^,\s]+)' \
+    --options-token 'OPTIONS:$' \
+    --options-filter '^\s\s\s(-\S[^,\s]+)|\s(-\S[^,\s]+)\s\s' \
+    -o completion -O > "$DIR/vke_completion.sh"
+
+cd "$PWD"
