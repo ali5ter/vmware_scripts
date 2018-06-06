69ce4de7b156899dd3a1e021f897fd290fc98569
Alister Lewis-Bowen <bowena@vmware.com>
2018-06-06 12:25:09 -0400

    identify bash completion

--- vke/create_completion.sh
+++ vke/create_completion.sh
@@ -12,17 +12,17 @@ cd ~/tmp
 mkdir -p cli_taxo
 cd cli_taxo
 curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/cli_taxo.py
-curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/completion.tmpl
+curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/bash_completion.tmpl
 curl -O https://raw.githubusercontent.com/ali5ter/cli_taxo/master/requirements.txt
 pip install -r requirements.txt
 chmod 755 cli_taxo.py
 
-## generate completion script and move into place
+## generate bash completion script and move into place
 ./cli_taxo.py vke \
     --commands-token '^COMMANDS:$' \
     --commands-filter '^\s\s\s\s\s(?!-)(\S[^,\s]+)' \
     --options-token 'OPTIONS:$' \
     --options-filter '^\s\s\s(-\S[^,\s]+)|\s(-\S[^,\s]+)\s\s' \
-    -o completion -O > "$DIR/vke_completion.sh"
+    -o bash -O > "$DIR/vke_bash_completion.sh"
 
 cd "$PWD"
