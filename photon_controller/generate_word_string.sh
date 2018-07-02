c187e06c9390726b6a4f07e63e73cc673671cfdb
Alister Lewis-Bowen <bowena@vmware.com>
2018-07-02 15:58:09 -0400

    better random number generation

--- photon_controller/generate_word_string.sh
+++ photon_controller/generate_word_string.sh
@@ -3,6 +3,9 @@
 # Cheesy word string generator
 # @author Alister Lewis-Bowen <alister@lewis-bowen.org>
 
+[[ -n $DEBUG ]] && set -x
+set -eou pipefail
+
 WORDFILE="/usr/share/dict/words"
 TOTAL_WORDS=$(awk 'NF!=0 {++c} END {print c}' $WORDFILE)
 NUM_WORDS=${1:-3}
@@ -10,7 +13,7 @@ DELIM=${2:--}
 
 string=''
 for i in $(seq $NUM_WORDS); do
-    rnum=$((RANDOM%TOTAL_WORDS+1))
+    rnum=$(perl -e 'print int rand $ARGV[0], "\n"; ' -- "$TOTAL_WORDS")
     word=$(sed -n "$rnum p" $WORDFILE)
     if [ "$string" == "" ]; then string="$word"
     else string="${string}${DELIM}$word"
