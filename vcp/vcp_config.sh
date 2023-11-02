c79a5160234bb76e6749345b8a58306362b565c8
Alister Lewis-Bowen <alister@lewis-bowen.org>
2023-11-02 15:38:27 -0400

    commit all changes

---
+++ vcp/vcp_config.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# @file vcp_config.sh
+# VMware Cloud PKS account settings and preferences
+# @author Alister Lewis-Bowen <alister@lewis-bowen.org>
+
+# shellcheck disable=SC2034
+VCP_TARGET='https://api.vcp.cloud.vmware.com'
+VCP_TOKEN='8698fa83-f29f-4354-aeb9-ac6c4a44bd1d'
+VCP_TOKEN='09711e6a-a53a-40da-b8b3-5cfffb4ece65'
+VCP_CSP_ORG_ID='fa2c1d78-9f00-4e30-8268-4ab81862080d'
+VCP_DEFAULT_FOLDER='UIDev'
+VCP_DEFAULT_PROJECT='ui-project'
+VCP_DEFAULT_REGION='us-east-1'
+VCP_CLUSTER_PREFIX='alb'
+VCP_CLUSTER_DNAME_PREFIX='alb-🦄'
+VCP_DEPLOYMENT_PREFIX='alb'
